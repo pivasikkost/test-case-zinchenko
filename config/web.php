b124f079a143e2933701a59c89887c1e5eb5a7a8
Konstantin Zosimenko <pivasikkost@gmail.com>
2019-03-27 08:02:08 +0300

    create 'orders' module

--- config/web.php
+++ config/web.php
@@ -7,6 +7,11 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'modules' => [
+        'orders' => [
+            'class' => 'app\modules\orders\Module',
+        ],
+    ],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
