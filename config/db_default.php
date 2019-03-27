9d404b88343972aad55c56402ed01d253dbe194c
Konstantin Zosimenko <pivasikkost@gmail.com>
2019-03-27 05:09:26 +0300

    configure and create a database

--- config/db_default.php
+++ config/db_default.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=test-case-zinchenko',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => 'root1',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
