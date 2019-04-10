9bea91e59d4dafd5eb788fff484bd2bc889a7157
Konstantin Zosimenko <pivasikkost@gmail.com>
2019-04-10 11:30:04 +0300

    remove extra code

--- modules/orders/controllers/OrdersController.php
+++ modules/orders/controllers/OrdersController.php
@@ -7,8 +7,6 @@ use app\modules\orders\models\Orders;
 use app\modules\orders\models\OrdersSearch;
 use app\modules\orders\models\Services;
 use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
 use yii\data\Pagination;
 
 /**
@@ -16,24 +14,10 @@ use yii\data\Pagination;
  */
 class OrdersController extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
-                ],
-            ],
-        ];
-    }
 
     /**
-     * Lists all Orders models.
-     * @return mixed
+     * Displays Orders.
+     * @return string the rendering result.
      */
     public function actionIndex()
     {
@@ -70,6 +54,9 @@ class OrdersController extends Controller
         ]);
     }
 
+    /**
+     * Export Orders to csv file.
+     */
     public function actionExport() {
         $data = "ID;User;Link;Quantity;Service;Status;Mode;Created\r\n";
 
@@ -96,84 +83,4 @@ class OrdersController extends Controller
         exit;
     }
 
-    /**
-     * Displays a single Orders model.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
-     * Creates a new Orders model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new Orders();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Updates an existing Orders model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Deletes an existing Orders model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the Orders model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Orders the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Orders::findOne($id)) !== null) {
-            return $model;
-        }
-
-        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
-    }
 }
