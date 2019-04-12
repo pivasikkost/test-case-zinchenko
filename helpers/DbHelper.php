2cdef13e1a27ae9a401754ad8cd9fc0b92fbfeed
Konstantin Zosimenko <pivasikkost@gmail.com>
2019-04-12 20:08:12 +0300

    fix initial migration

---
+++ helpers/DbHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\helpers;
+
+/**
+ * DbHelper provides functionality to get specific information from the database.
+ *
+ * @author Konstantin Zosimenko <pivasikkost@gmail.com>
+ * @since 1.0
+ */
+class DbHelper
+{
+    /**
+     * @param string $name
+     * @param string $dsn
+     *
+     * @return string
+     */
+    public static function getDsnAttribute($name, $dsn)
+    {
+        if (preg_match('/' . $name . '=([^;]*)/', $dsn, $match)) {
+            return $match[1];
+        } else {
+            return null;
+        }
+    }
+}
